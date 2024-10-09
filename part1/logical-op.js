a4eb872f9a2303131a4c7e913d24b1944b130e7c
KuldeepJha5176 <ranacjha@gmail.com>
2024-10-09 23:06:04 +0530

    .gitexploring more into js

---
+++ part1/logical-op.js
@@ -0,0 +1,10 @@
+//&& - and
+//|| - or
+//! - reverse
+let isloggedin = true
+let ispaid = false
+
+console.log(isloggedin && ispaid);
+
+console.log(isloggedin || ispaid);
+ // to overcome operator presedence use () 
