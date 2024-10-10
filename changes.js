a598ea38ba0b840b6e06c275b602f720036b014e
KuldeepJha5176 <ranacjha@gmail.com>
2024-10-10 22:23:39 +0530

    Add files via upload
    learning

---
+++ changes.js
@@ -0,0 +1,13 @@
+let gameName = "cricket";
+
+gameName = "batman"
+
+console.log(gameName);
+
+const username = "rana" //when the devlopers dont want to change the variable or dont want to reassign  
+
+username = "kuldeep";
+
+console.log(username);
+
+
---
+++ datatypes.js
@@ -0,0 +1,12 @@
+let score = 102;
+let name ="Rana"
+let islogged = true;
+
+//object
+let teatypes = ['lemon tea','orange tea']
+<<<<<<< HEAD
+let user = {firstname: "kuldeep", lastname:"jha"} 
+console.log(score);
+=======
+let user = {firstname: "kuldeep", lastname ="jha"}
+>>>>>>> 78ced36e3dd57fdef3b4463a1fd87179760e3207
